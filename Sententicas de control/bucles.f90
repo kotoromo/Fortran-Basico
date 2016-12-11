8a3b0ea914dd1866b7b2b7f97b64eaf3db5f88bb
Nicky García <nick.garc.96@gmail.com>
2016-12-10 20:02:52 -0600

    Sentencias de Control done

--- Sententicas de control/bucles.f90
+++ Sententicas de control/bucles.f90
@@ -1,4 +1,32 @@
 program bucles
+integer i, j
+
+!Los bucles son una herramienta especialmente útil cuando es necesario repetir alguna sentencia o grupo de sentencias
+!de manera ciclica. Fortran nos permite el uso de bucles do (similar al ciclo for de otros lenguajes) y el bucle
+!do-while
+
+!Supongase que se tiene el siguiente arreglo:
+integer pi(5)
+data pi /3,1,4,1,6/
+
+!Si quisieramos imprimirlo, veremos que tendriamos que nombrar cada uno de sus elementos uno por uno e imprimirlos:
+!print *, pi(1)
+!print *, pi(2)
+!	....
+
+!Si quisieramos agilizar este proceso, podemos utilizar un bucle do:
+do i=1, size(pi)
+print *, "Pi: ",pi(i)
+end do
+
+!Si quisieramos un bucle que haga algo hasta que una condición sea verdadera, hacemos uso de el bucle do-while:
+i = 1
+do while (i .ne. size(pi)+1)
+print *, "Pi_2: ", pi(i)
+i = i + 1
+end do
+
+!Este bucle hace lo mismo que el anterior.
 
 !También se puede utilizar algo llamado "Implied DO" para construir arreglos de dimensión 1
 !Utilizando la siguiente sintaxis:
@@ -10,7 +38,19 @@ program bucles
 !entre np y nf; Si se omite nc, toma el valor de 1.
 
 !Ejemplo:
-integer k
-(0, (-1)**k, k=0, 2) !Esto da 0, 1, 0, -1, 0, 1
+print *, (0, (-1)**k, k=0, 2)!Esto da 0, 1, 0, -1, 0, 1
+
+print *, (0, (0, -1)**k, k=0, 2)
+
+!Estos ciclos se pueden anidar:
+
+print *, (i*k, (j*j, i*j, j=1, 3), i=2, 4)
+
+print *, (1+k, (1, j**k, j=1, 2), k = 1, 2)
+
+!Por último, podemos controlar el flujo del bucle con las instrucciones:
+! exit: Sale instantaneamente del bucle
+! cycle: El programa continua at inicio de la siguiente iteración
+! stop: El programa detiene su ejecución.
 
 end program bucles
---
+++ Sententicas de control/if.f90
@@ -0,0 +1,15 @@
+program if
+!Una sentencia if..then
+i = 2
+if (i .eq. 2) then
+print *, "i == 2"
+end if
+
+!i = 3
+if (i .eq. 3) then
+	print *, "i == 3"
+else
+	print *, "i /= 3"
+end if
+
+end program if
